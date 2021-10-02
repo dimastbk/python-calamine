9f4d14b4be8ee03734f6f2ff6e147b0fbb4653d6
Дмитрий <dimastbk@yandex.ru>
2021-10-02 21:12:41 +1000

    added support to parse time/date/datetime from xlsx

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,18 @@
+use chrono;
+use chrono::{Datelike, Timelike};
 use pyo3::prelude::*;
+use pyo3::types::{PyDate, PyDateTime, PyTime};
 
 #[derive(Debug)]
 pub enum CellValue {
     Int(i64),
     Float(f64),
     String(String),
-    DateTime(f64),
+    Time(chrono::NaiveTime),
+    Date(chrono::NaiveDate),
+    DateTime(chrono::NaiveDateTime),
     Bool(bool),
-    None,
+    Empty,
 }
 
 impl IntoPy<PyObject> for CellValue {
@@ -17,8 +22,33 @@ impl IntoPy<PyObject> for CellValue {
             CellValue::Float(v) => v.to_object(py),
             CellValue::String(v) => v.to_object(py),
             CellValue::Bool(v) => v.to_object(py),
-            CellValue::DateTime(v) => v.to_object(py),
-            CellValue::None => "".to_object(py),
+            CellValue::Time(v) => PyTime::new(
+                py,
+                v.hour() as u8,
+                v.minute() as u8,
+                v.second() as u8,
+                (v.nanosecond() / 1000) as u32,
+                None,
+            )
+            .unwrap()
+            .to_object(py),
+            CellValue::Date(v) => PyDate::new(py, v.year(), v.month() as u8, v.day() as u8)
+                .unwrap()
+                .to_object(py),
+            CellValue::DateTime(v) => PyDateTime::new(
+                py,
+                v.year(),
+                v.month() as u8,
+                v.day() as u8,
+                v.hour() as u8,
+                v.minute() as u8,
+                v.second() as u8,
+                v.timestamp_subsec_micros(),
+                None,
+            )
+            .unwrap()
+            .to_object(py),
+            CellValue::Empty => "".to_object(py),
         }
     }
 }
