b73c706506240c9019cd39e25bdf88df780a961f
Дмитрий <dimastbk@yandex.ru>
2021-10-06 11:41:03 +1000

    fix lint

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,31 +3,31 @@ from pathlib import Path
 
 from python_calamine import get_sheet_data, get_sheet_names
 
-PATH = Path(__file__).parent / 'data'
+PATH = Path(__file__).parent / "data"
 
 
 def test_ods_read():
     # calamine not supported ods date/datetime parse
-    names = ['Sheet1', 'Sheet2']
-    data = [['String', 1, 1.1, True, False]]
+    names = ["Sheet1", "Sheet2"]
+    data = [["String", 1, 1.1, True, False]]
 
-    assert names == get_sheet_names((PATH / 'base.ods').as_posix())
-    assert data == get_sheet_data((PATH / 'base.ods').as_posix(), 0)
+    assert names == get_sheet_names((PATH / "base.ods").as_posix())
+    assert data == get_sheet_data((PATH / "base.ods").as_posix(), 0)
 
 
 def test_xls_read():
     # calamine not supported xls date/datetime parse
-    names = ['Sheet1', 'Sheet2']
-    data = [['String', 1, 1.1, True, False]]
+    names = ["Sheet1", "Sheet2"]
+    data = [["String", 1, 1.1, True, False]]
 
-    assert names == get_sheet_names((PATH / 'base.xls').as_posix())
-    assert data == get_sheet_data((PATH / 'base.xls').as_posix(), 0)
+    assert names == get_sheet_names((PATH / "base.xls").as_posix())
+    assert data == get_sheet_data((PATH / "base.xls").as_posix(), 0)
 
 
 def test_xlsx_read():
     # calamine not supported xlsx datetime parse
-    names = ['Sheet1', 'Sheet2']
-    data = [['String', 1, 1.1, True, False, date(2020, 1, 1)]]
+    names = ["Sheet1", "Sheet2"]
+    data = [["String", 1, 1.1, True, False, date(2020, 1, 1)]]
 
-    assert names == get_sheet_names((PATH / 'base.xlsx').as_posix())
-    assert data == get_sheet_data((PATH / 'base.xlsx').as_posix(), 0)
+    assert names == get_sheet_names((PATH / "base.xlsx").as_posix())
+    assert data == get_sheet_data((PATH / "base.xlsx").as_posix(), 0)
